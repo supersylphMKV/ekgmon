01be7c26b7426a8bc1f0fb8335b87c0a66a82518
henri wijaya <henri.wijaya@tes.co.id>
2018-10-17 13:22:47 +0700

    add webhome

--- server/public/javascripts/ecgMonHome.js
+++ server/public/javascripts/ecgMonHome.js
@@ -18,7 +18,7 @@ function SetUp(){
 
 function GetDataList(){
     socket.emit('list',{table : 'r'}, function(err,res){
-        console.log(res);
+        //console.log(res);
         res.sort(compare);
         if(res){
             var dataKeys = Object.keys(res);
@@ -27,14 +27,35 @@ function GetDataList(){
                 $(childELm).find(".td_name").text(res[dataKeys[i]].name);
                 $(childELm).find(".td_age").text(res[dataKeys[i]].age);
                 $(childELm).find(".td_date").text(res[dataKeys[i]].date);
+                $(childELm).find(".btn_submit").on('click',res[dataKeys[i]].data, PreviewData);
                 $("#tableData-body").append($(childELm));
             }
         }
     })
 }
 
-function PreviewData(){
-
+function PreviewData(d){
+    var data = d.data;
+    //console.log(data);
+    var c = $('#monitor')[0];
+    var ctx = c.getContext('2d');
+    $('#monitor').attr('width', data.length * 5 + 'px');
+    $('#monitor').attr('height', '1024px');
+    ctx.beginPath();
+    ctx.strokeStyle = "yellow";
+    for(var i = 0; i < data.length-1; i++){
+        var d = -data[i] + 300;
+        if(i > 0 && data[i] == 0){
+            d = -data[i-1] + 300;
+        }
+        if(i==0){
+            ctx.moveTo(0,d);
+            console.log("start draw")
+        }
+        ctx.lineTo(i*5, d);
+        console.log(data[i]);
+    }
+    ctx.stroke();
 }
 
 function compare(a,b) {
@@ -42,7 +63,7 @@ function compare(a,b) {
     var mb = MonthToInt(b.date.split(' ')[1]);
     var sa = ma + " " + a.date.split(' ').slice(2).join(' ');
     var sb = mb + " " + b.date.split(' ').slice(2).join(' ');
-    console.log(sa, sb);
+    //console.log(sa, sb);
     if (sa < sb)
         return 1;
     if (sa > sb)
